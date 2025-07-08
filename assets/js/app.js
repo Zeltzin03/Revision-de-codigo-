c36dd9a81c2e9552f36e8297f04d88b1e64258fd
Zeltzin03 <zeltzinromo@mail.com>
2025-07-07 18:52:26 -0600

    Corrección de errores

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,18 +1,28 @@
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
+//selecctores correctos
+const $n = document.querySelector('.name');
+const $b = document.querySelector('.blog');
 const $l = document.querySelector('.location');
 
-function displayUser(username) {
+//faltaba función
+async function displayUser(username) {
   $n.textContent = 'cargando...';
+  try {
   const response = await fetch(`${usersEndpoint}/${username}`);
+  //validar
+  if (!response.ok){
+    throw new Error(`Error HTTP: ${response.status}`);
+  }
+  const data = await response.json();//convertir datos a JSON
   console.log(data);
   $n.textContent = '${data.name}';
   $b.textContent = '${data.blog}';
   $l.textContent = '${data.location}';
+}catch (error){
+    handleError(err);
 }
-
+}
 function handleError(err) {
   console.log('OH NO!');
   console.log(err);
